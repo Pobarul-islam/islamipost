e3a634d871f2072d7e80afda18a906facef2c3d1
Pobarul-islam <pobarul3@gmail.com>
2022-06-24 19:22:57 +0600

    islamipost started

---
+++ firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCxBYn3lxnCFxHBMRn3jpG88lLQ8Htf8fA",
+  authDomain: "islamipost-15fde.firebaseapp.com",
+  projectId: "islamipost-15fde",
+  storageBucket: "islamipost-15fde.appspot.com",
+  messagingSenderId: "260409490756",
+  appId: "1:260409490756:web:67cf50607d3fd462e6eb11"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+          <div className="header_left"></div>
+
+          <div className="header_middle"></div>
+
+          <div className="header_right"></div>
+        </div>
+    );
+};
+
+export default Header;
